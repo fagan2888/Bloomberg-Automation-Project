1c05f3f0fd2a8a51e19c202005a0499f7d28c97e
Weixi Zhuo <zhuowei@americas.manulife.net>
2019-11-08 14:48:39 -0500

    Config setup

--- BloombergAutomation/BloombergAutomation/BloombergAutomation.py
+++ BloombergAutomation/BloombergAutomation/BloombergAutomation.py
@@ -1,6 +1,12 @@
-import blpapi 
-from xbbg import blp
+##import blpapi 
+##import pdblp as blp
+import json
 
+##Configuration Stage
+with open("Configuration.json") as json_file:
+    basic_config=json.load(json_file)
+
+print(basic_config)
 
 
 
